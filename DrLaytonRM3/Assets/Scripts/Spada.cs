409930b05e7235f49553c4a83daae324686c9e68
nenelontano <irene.lontano@gmail.com>
2023-03-16 16:58:43 +0100

    modificato script spada
    aggiunta parte di codice per muovere la spada con tastiera tramite lo 'spazio' (non funziona proprio come dovrebbe) 4.35

--- DrLaytonRM3/Assets/Scripts/Spada.cs
+++ DrLaytonRM3/Assets/Scripts/Spada.cs
@@ -2,52 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spada : Collidable
-{
- //Damage struct
- public int damagePoint=1;
- public float pushForce=2.0f;
+public class Spada : Collidable {
+ 
+    //Damage struct
+    public int damagePoint=1;
+    public float pushForce=2.0f;
 
- //Upgrade
- public int weaponLevel=0;
- private SpriteRenderer SpriteRenderer;
+    //Upgrade
+    public int weaponLevel=0;
+    private SpriteRenderer SpriteRenderer;
 
- //Swing
- private float cooldown=0.5f;
- private float lastSwing;
+    //Swing
+    private Animator anim;
+    private float cooldown=0.5f;
+    private float lastSwing;
 
- protected override void Start(){
-    base.Start();
-    SpriteRenderer=GetComponent<SpriteRenderer>();
- }
+    protected override void Start(){
+        base.Start();
+        SpriteRenderer=GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
 
- protected override void UpDate(){
-    base.UpDate();
-    if(Input.GetKeyDown(KeyCode.Space)){
-        if(Time.time-lastSwing>cooldown){
-            lastSwing=Time.time;
-            Swing();
+    protected override void UpDate(){
+        base.UpDate();
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(Time.time-lastSwing>cooldown){
+                lastSwing=Time.time;
+                Swing();
+            }
         }
     }
- }
 
-private void OnTriggerEnter2D(Collider2D other){
-    if(other.CompareTag("Fighter")){
-        if(other.name=="Player")
-            return;
-    
-        //Create a new Damage object, then we'll send it to the fighter we've hit
-        Damage dmg=new Damage{
-            damageAmount=damagePoint,
-            origin=transform.position,
-            pushForce=pushForce
-        };
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Fighter")){
+            if(other.name=="Player")
+                return;
+        
+            //Create a new Damage object, then we'll send it to the fighter we've hit
+            Damage dmg=new Damage{
+                damageAmount=damagePoint,
+                origin=transform.position,
+                pushForce=pushForce
+            };
 
-        other.SendMessage("ReceiveDamage", dmg);
+            other.SendMessage("ReceiveDamage", dmg);
+        }
     }
-}
 
- private void Swing(){
-    Debug.Log("Swing");
- }
+    private void Swing() {
+       
+        anim.SetTrigger("Swing");
+
+
+    }
 }
