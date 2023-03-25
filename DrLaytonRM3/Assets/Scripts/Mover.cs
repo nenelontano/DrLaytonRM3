68dd6855cdce8011cb43e5155170a136349ee134
martinaligorio <mar.ligorio@stud.uniroma3.it>
2023-03-25 16:16:37 +0100

    modificato animation spada
    (7.45)

--- DrLaytonRM3/Assets/Scripts/Mover.cs
+++ DrLaytonRM3/Assets/Scripts/Mover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public abstract class Mover : Fighter {
     
+    private Vector3 originalSize;
     protected BoxCollider2D boxCollider;
     protected Vector3 moveDelta;
     protected RaycastHit2D hit;
@@ -12,6 +13,7 @@ public abstract class Mover : Fighter {
 
     protected virtual void Start()       
     {
+        originalSize=transform.localScale;
         boxCollider = GetComponent<BoxCollider2D>();
     }
     
@@ -23,9 +25,9 @@ public abstract class Mover : Fighter {
 
         //Swap sprite directions 
         if(moveDelta.x > 0)
-            transform.localScale = Vector3.one;
+            transform.localScale = originalSize;
         else if(moveDelta.x < 0)
-            transform.localScale =new Vector3(-1,1,1);
+            transform.localScale =new Vector3(originalSize.x*-1,originalSize.y,originalSize.z);
 
         //add push vector, if any
         moveDelta += pushDirection;
