ce1fa1ac9fc897d62dadd25f9a2019b5e5327886
nenelontano <irene.lontano@gmail.com>
2023-07-21 17:27:28 +0200

    Revert "Revert "cambiati mostri scena iniziale""
    This reverts commit 3a7c51707e216200dadaee5ea5560db7dee8e5b7.

--- DrLaytonRM3/Assets/Scripts/NPCTextPerson.cs
+++ DrLaytonRM3/Assets/Scripts/NPCTextPerson.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class NPCTextPerson : Collidable {
     
     public string message;
+    public string message2;
+    public string message3;
+    public string message4;
+    public string message5;
 
     private float cooldown = 4.0f;
     private float lastShout;
@@ -19,7 +23,12 @@ public class NPCTextPerson : Collidable {
 
         if(Time.time - lastShout > cooldown)  {
             lastShout = Time.time;
-            GameManager.instance.showText(message, 25, Color.white, transform.position + new Vector3(0, 0.16f, 0), Vector3.zero, cooldown);
+            GameManager.instance.showText(message, 25, Color.white, transform.position + new Vector3(0, 0.50f, 0), Vector3.zero, cooldown);
+            GameManager.instance.showText(message2, 25, Color.white, transform.position + new Vector3(0, 0.40f, 0), Vector3.zero, cooldown);
+            GameManager.instance.showText(message3, 25, Color.white, transform.position + new Vector3(0, 0.30f, 0), Vector3.zero, cooldown);
+            GameManager.instance.showText(message4, 25, Color.white, transform.position + new Vector3(0, 0.20f, 0), Vector3.zero, cooldown);
+            GameManager.instance.showText(message5, 25, Color.white, transform.position + new Vector3(0, 0.10f, 0), Vector3.zero, cooldown);
+
         }
     }
 }
